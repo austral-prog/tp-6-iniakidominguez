e78dce8bf16342f753d51a864410894a2d4f3a2c
iniakidominguez <73805462+iniakidominguez@users.noreply.github.com>
2025-05-13 17:14:11 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,46 +1,24 @@
-def remove_elements(lista):
-    for i in sorted([5, 4, 0], reverse=True):
-        if i < len(lista):
-         del lista[i]
-    return lista
+def remove_elements(lst):
+    lst_copy = lst[:]
+    for i in sorted([0, 4, 5], reverse=True):
+        if i < len(lst_copy):
+            del lst_copy[i]
+    return lst_copy
 
-def add_elements(list_to_add_elements):
-    list_to_add_elements.insert(0,"Pink")
-    list_to_add_elements.append("Yellow")
-    return list_to_add_elements
+def add_elements(lst):
+    return ['Pink'] + lst + ['Yellow']
 
-def is_empty(list_to_check):
-    if len(list_to_check) == 0:
-        return True
-    else:
-        return False
+def is_empty(lst):
+    return len(lst) == 0
 
-def check_lists(list_to_compare1, list_to_compare2):
-    if len(list_to_compare1)<3 or len(list_to_compare2)<3:
-        return False
-    else :
-        if list_to_compare1[2] == list_to_compare2[2]:
-            return True
-        else:
-            return False
+def check_lists(lst1, lst2):
+    if len(lst1) > 2 and len(lst2) > 2:
+        return lst1[2] == lst2[2]
+    return False
 
 def list_of_lists(list_of_lists_to_modify):
-    if len(list_of_lists_to_modify[0]) < 3:
-        paso_3 = list_of_lists_to_modify[0]
-    else:
-        paso_3 = list_of_lists_to_modify[0][0:2]
-
-    if len(list_of_lists_to_modify[1]) < 5:
-        if len(list_of_lists_to_modify[1]) >= 2:
-            paso_2 = list_of_lists_to_modify[1][1:]
-        else:
- paso_2 = []
-    else:
-        paso_2 = list_of_lists_to_modify[1][1:4]
-
-    if len(list_of_lists_to_modify[2]) < 3:
-        paso_1 = list_of_lists_to_modify[2]
-    else:
-        paso_1 = list_of_lists_to_modify[2][-2:]
-
-    return [paso_3, paso_2, paso_1]
+    return [
+        list_of_lists_to_modify[0][:2],
+        list_of_lists_to_modify[1][1:4],
+        list_of_lists_to_modify[2][-2:]
+    ]
